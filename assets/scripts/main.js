7f040dd7b5ec24826ea6df8be47577ab6cfb0e54
robzgf <rgrielfilho@gmail.com>
2021-01-11 21:33:22 -0300

    Menu e Auth

---
+++ assets/scripts/main.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body");
+const btnOpen = document.querySelector("#btn-open");
+const closeMenu = document.querySelectorAll('[data-close = "menu"]');
+const menuLinks = document.querySelectorAll(".menu a");
+
+if (menuLinks) {
+    menuLinks.forEach(el => {
+        el.addEventListener("click", (e)=>{
+            body.classList.remove('open-menu');
+        });
+    });
+}
+
+
+if (btnOpen) {
+    btnOpen.addEventListener('click', (e)=>{
+        body.classList.add('open-menu');
+    });
+}
+
+if (closeMenu) {
+
+    closeMenu.forEach(el => {
+        el.addEventListener('click', (e)=>{
+            body.classList.remove('open-menu');
+        });
+    });
+
+    
+}
