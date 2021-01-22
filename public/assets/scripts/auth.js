d6d77157a5777aa0d748abb7aa432bb3a112d6b2
robzgf <rgrielfilho@gmail.com>
2021-01-21 22:15:34 -0300

    Reset form fixed

--- public/assets/scripts/auth.js
+++ public/assets/scripts/auth.js
@@ -182,7 +182,10 @@ if (authPage) {
 
         auth.verifyPasswordResetCode(oobCode)
         .then(()=>auth.confirmPasswordReset(oobCode, password))
-        .then(()=>showAuthForm('login'))
+        .then(()=>{
+            hideAuthForms();
+            showAuthForm('login');
+        })
         .catch(showAlertError(formReset))
         .finally(()=>{
             btnSubmit.disabled = false;
